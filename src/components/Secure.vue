f90b43abe4259dac4d3fa1ca9e538230a0932917
Kemari <kemaric@gmail.com>
2019-05-05 00:46:40 -0400

    Added auth.

--- src/components/Secure.vue
+++ src/components/Secure.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <h1>Secure Page</h1>
+  <div class='hello'>
+    <h1 class='view-header'>Secure Page</h1>
+    <h5>Data from GET /secure-data:</h5>
+    <div class="results">
+      <pre>{{ data }}</pre>
+    </div>
     <div>
       <router-link to="/">Go back</router-link>
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      data: null,
+    };
+  },
+  async mounted() {
+    const accessToken = await this.$auth.getAccessToken();
+    const client = axios.create({
+      baseURL: 'http://localhost:8081',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const { data } = await client.get('/secure-data');
+    this.data = data;
+  },
+};
+</script>
 
