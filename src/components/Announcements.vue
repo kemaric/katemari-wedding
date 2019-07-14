82e1edb8d1176dffa958160861172f53e0e6677e
Kemari <kemaric@gmail.com>
2019-07-13 23:00:10 -0400

    Initisl firebase basic integration.

--- src/components/Announcements.vue
+++ src/components/Announcements.vue
@@ -3,29 +3,69 @@
     <h1 class="component-head">{{ msg }}</h1>
     <!-- <h2>Latest announcements</h2> -->
     <ul class="announcement-list">
-      <li class="announcement" v-for='announcement in announcements' v-bind:key='announcement'>
-        <span> {{ announcement }}</span>
+      <li class="announcement" v-for="announcement in announcements" v-bind:key="announcement">
+        <span>{{ announcement.text }}</span>
       </li>
-      <li v-if='announcements.length === 0'> 
-        <span>
-          {{ noAnnouncement }}
-         </span> 
+      <li v-if="announcements.length === 0">
+        <span>{{ noAnnouncement }}</span>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { db } from '../main';
+
 export default {
   name: 'announcements',
+  mounted() {
+    this.loadNews();
+  },
   data() {
     return {
       msg: 'Latest announcements',
-      noAnnouncement: 'Stay tuned for upcoming #KatemariWedding Announcements...',
-      announcements: [
-        'The wedding Save the Dates have been sent out!',
-      ],
+      noAnnouncement:
+        'Stay tuned for upcoming #KatemariWedding Announcements...',
+      announcements: [],
     };
+  },
+  firestore() {
+    if (db) {
+      return {
+        announcements: db
+          .collection('announcements')
+          .orderBy('date_made', 'desc'),
+      };
+    }
+    return { announcements: [] };
+  },
+  methods: {
+    loadNews() {
+      if (!db) {
+        return { announcements: [] };
+      }
+      // Create the query to load the last 12 messages and listen for new ones.
+      const query = db
+        .collection('announcements')
+        .orderBy('date_made', 'desc')
+        .limit(12);
+
+      return query.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === 'removed') {
+            console.log('Entry removed', change.doc.id, change.doc.data());
+            // deleteMessage(change.doc.id);
+            return change.doc.data();
+          }
+          const message = change.doc.data();
+          console.log('Entry changed', change.doc.id, message);
+          return change.doc.data();
+          // displayMessage(change.doc.id, message.timestamp, message.name,
+          //               message.text, message.profilePicUrl, message.imageUrl);
+        },
+        );
+      });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { firestorePlugin } from 'vuefire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import App from './App';
 import router from './router';
 
+Vue.use(firestorePlugin);
+firebase.initializeApp({
+  projectId: 'katemariwedding-134ce',
+  databaseURL: 'https://katemariwedding-134ce.firebaseio.com',
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -13,3 +21,6 @@ new Vue({
   template: '<App/>',
   components: { App },
 });
+
+export const db = firebase.firestore();
+export const myBoi = 'im here to remove a warning';
